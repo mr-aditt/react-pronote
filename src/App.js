5b356f6bce22661fc7134ac9ad79a6b0fe2db161
mr-aditt <mr.aditt@outlook.com>
2021-12-07 12:35:00 +0530

    Final commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import EDITOR from './Components/Editor'
+
+
+function App() {
+  return (
+    <div className="App">
+      <EDITOR/>
+    </div>
+  );
+}
+
+export default App;
